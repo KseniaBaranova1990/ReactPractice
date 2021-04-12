5ee777f81ddf5ed8a54335034776599f9b7f2b37
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-04-12 18:06:32 +0300

    setState, onClick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class WhoAmI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 26,
+    };
+    this.nextYear = this.nextYear.bind(this);
+  }
+  nextYear() {
+    this.setState((state) => ({
+      years: ++state.years,
+    }));
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { years } = this.state;
+    return (
+      <>
+        <h1>
+          My name is {name} my surname is {surname} years={years}
+        </h1>
+        <button onClick={this.nextYear}>++</button>
+        <a href={link}>My Profile</a>
+      </>
+    );
+  }
 }
 
-export default App;
+const All = () => {
+  return (
+    <>
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Mike" surname="Brown" link="facebook.com" />
+      <WhoAmI name="Kate" surname="Wispa" link="facebook.com" />
+    </>
+  );
+};
+
+export default All;
